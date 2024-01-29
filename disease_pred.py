696abe0b3f32b110e20da756b039c9f5e3031857
Mohamed Ali Talbe <alitalbe33@gmail.com>
2024-01-28 19:42:34 -0500

    Reached Model building

--- disease_pred.py
+++ disease_pred.py
@@ -4,18 +4,80 @@ import numpy as np
 import pandas as pd
 #Draw visualisations
 import matplotlib.pyplot as plt
-import seaborn as sb
+import seaborn as sns
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn import metrics
+from scipy.stats import mode
+
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVC
-from xgboost import XGBClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from imblearn.over_sampling import RandomOverSampler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
 
-import warnings
-warnings.filterwarnings('ignore')
+# import warnings
+# warnings.filterwarnings('ignore')
 
-df = pd.read_csv('Rainfall.csv')
-df.head()
+
+# Reading the train.csv by removing the 
+# last column since it's an empty column
+DATA_PATH = "dataset/Training.csv"
+data = pd.read_csv(DATA_PATH).dropna(axis = 1)
+
+print(type(data))
+
+# Checking whether the dataset is balanced or not
+disease_counts = data["prognosis"].value_counts()
+temp_df = pd.DataFrame({
+    "Disease": disease_counts.index,
+    "Counts": disease_counts.values
+})
+ 
+plt.figure(figsize = (18,8))
+sns.barplot(x = "Disease", y = "Counts", data = temp_df)
+plt.xticks(rotation=90)
+plt.show()
+
+
+# Encoding the target value into numerical
+# value using LabelEncoder
+encoder = LabelEncoder()
+data["prognosis"] = encoder.fit_transform(data["prognosis"])
+
+
+
+#Splitting the data for training and testing (80/20 split)
+X = data.iloc[:,:-1]
+y = data.iloc[:, -1]
+X_train, X_test, y_train, y_test =train_test_split(
+  X, y, test_size = 0.2, random_state = 24)
+ 
+print(f"Train: {X_train.shape}, {y_train.shape}")
+print(f"Test: {X_test.shape}, {y_test.shape}")
+
+
+
+# Defining scoring metric for k-fold cross validation
+def cv_scoring(estimator, X, y):
+    return accuracy_score(y, estimator.predict(X))
+
+# Initializing Models
+models = {
+    "SVC":SVC(),
+    "Gaussian NB":GaussianNB(),
+    "Random Forest":RandomForestClassifier(random_state=18)
+}
+ 
+# Producing cross validation score for the models
+for model_name in models:
+    model = models[model_name]
+    scores = cross_val_score(model, X, y, cv = 10, 
+                             n_jobs = -1, 
+                             scoring = cv_scoring)
+    print("=="*30)
+    print(model_name)
+    print(f"Scores: {scores}")
+    print(f"Mean Score: {np.mean(scores)}")
